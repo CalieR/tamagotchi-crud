4b5da2bd678d09040d7af38828102f3870c38718
Calie Rushton <calielrushton@gmail.com>
2024-10-19 13:31:31 +0100

    Footer component placeholder

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-100 text-base-content p-4 sticky bottom-0">
+      <div className="container">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by CR
+            Industries Ltd
+          </p>
+        </aside>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
